8c6514a64746ffc3027663de0cc242cc234dc5c4
NateJose123 <josen@berea.edu>
2021-01-15 15:06:45 -0600

    Initial Commit

---
+++ Instagram/Controller/MainTabController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabController.swift
+//  Instagram
+//
+//  Created by Nathan Jose on 1/15/21.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+}
